597e7946b3fefcedae322966c32394769555f908
Liam Wynn <wynnliam@pdx.edu>
2019-06-16 14:05:54 -0700

    Added simple multiuser support

---
+++ server.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var express = require('express');
+var socket = require('socket.io');
+
+var server = express();
+
+var io = socket(server.listen(process.env.PORT || 8080));
+
+io.on('connection', function(objectSocket) {
+	objectSocket.on('paint', function(paintData) {
+		io.emit('renderPixel', paintData);
+	});
+});
